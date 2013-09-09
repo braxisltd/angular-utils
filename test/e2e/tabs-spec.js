59e497ece068cc03fc837f49e13a14ed7cdab60c
Darren Ballinger <darren.ballinger@gmail.com>
2013-09-09 07:25:38 +0100

    Added tests around using the tabs directives as attributes

--- test/e2e/tabs-spec.js
+++ test/e2e/tabs-spec.js
@@ -2,49 +2,105 @@
 
 describe('tabs', function () {
 
-    beforeEach(function () {
-        browser().navigateTo('/app/tabs.html');
+    describe('using element', function () {
+
+        beforeEach(function () {
+            browser().navigateTo('/app/tabs.html');
+            scopedSelector = '#element';
+        });
+
+        assertions();
     });
 
-    it('should have an unordered list for the headings', function () {
-        expect(element('ul.headings').count()).toEqual(1);
+    describe('using element', function () {
+
+        beforeEach(function () {
+            browser().navigateTo('/app/tabs.html');
+            scopedSelector = '#attribute';
+        });
+
+        assertions();
     });
 
-    it('should have an li list each tab', function () {
-        expect(element('ul.headings li').count()).toEqual(2);
-    });
+    var scopedSelector;
 
-    it('should have an a in each li contnaining the heading text', function () {
-        expect(element('ul.headings li a:eq(0)').text()).toContain('heading 1');
-        expect(element('ul.headings li a:eq(1)').text()).toContain('heading 2');
-    });
+    function scoped() {
+        return using(scopedSelector);
+    }
 
-    it('should have 2 panes', function () {
-        expect(element('div.tab-content:eq(0)').text()).toContain('content 1');
-        expect(element('div.tab-content:eq(1)').text()).toContain('content 2');
-    });
+    function assertions() {
+        it('should have an unordered list for the headings', function () {
+            assertUl();
+        });
 
-    it('static classes should be pushed onto the headings', function () {
-        expect(element('.head1').text()).toContain('heading 1');
-    });
+        it('should have an li list each tab', function () {
+            assertLi();
+        });
 
-    it('dynamic classes should be pushed onto the headings', function () {
-        expect(element('.head2').text()).toContain('heading 2');
-    });
+        it('should have an a in each li contnaining the heading text', function () {
+            assertHeadingText();
+        });
 
-    it('first tab should default to active and only content for the first pane should be visible', function () {
-        expect(element('li.active').count()).toEqual(1);
-        expect(element('li.active').text()).toContain('heading 1');
-        expect(element('div.tab-content:visible').count()).toEqual(1);
-        expect(element('div.tab-content:visible').text()).toContain('content 1');
-    });
+        it('should have 2 panes', function () {
+            assertPanes();
+        });
 
-    it('clicking on the second tab should switch behaviour', function () {
-        element('.headings a:eq(1)').click();
-        expect(element('li.active').count()).toEqual(1);
-        expect(element('li.active').text()).toContain('heading 2');
-        expect(element('div.tab-content:visible').count()).toEqual(1);
-        expect(element('div.tab-content:visible').text()).toContain('content 2');
-    });
+        it('static classes should be pushed onto the headings', function () {
+            assertStaticClasses();
+        });
+
+        it('dynamic classes should be pushed onto the headings', function () {
+            assertDynamicClasses();
+        });
+
+        it('first tab should default to active and only content for the first pane should be visible', function () {
+            assertDefualtToFirstTab();
+        });
+
+        it('clicking on the second tab should switch behaviour', function () {
+            assertSwicthTabs();
+        });
+    }
+
+    function assertUl() {
+        expect(scoped().element('ul.headings').count()).toEqual(1);
+    }
+
+    function assertLi() {
+        expect(scoped().element('ul.headings li').count()).toEqual(2);
+    }
+
+    function assertHeadingText() {
+        expect(scoped().element('ul.headings li a:eq(0)').text()).toContain('heading 1');
+        expect(scoped().element('ul.headings li a:eq(1)').text()).toContain('heading 2');
+    }
+
+    function assertPanes() {
+        expect(scoped().element('div.tab-content:eq(0)').text()).toContain('content 1');
+        expect(scoped().element('div.tab-content:eq(1)').text()).toContain('content 2');
+    }
+
+    function assertStaticClasses() {
+        expect(scoped().element('.head1').text()).toContain('heading 1');
+    }
+
+    function assertDynamicClasses() {
+        expect(scoped().element('.head2').text()).toContain('heading 2');
+    }
+
+    function assertDefualtToFirstTab() {
+        expect(scoped().element('li.active').count()).toEqual(1);
+        expect(scoped().element('li.active').text()).toContain('heading 1');
+        expect(scoped().element('div.tab-content:visible').count()).toEqual(1);
+        expect(scoped().element('div.tab-content:visible').text()).toContain('content 1');
+    }
+
+    function assertSwicthTabs() {
+        scoped().element('.headings a:eq(1)').click();
+        expect(scoped().element('li.active').count()).toEqual(1);
+        expect(scoped().element('li.active').text()).toContain('heading 2');
+        expect(scoped().element('div.tab-content:visible').count()).toEqual(1);
+        expect(scoped().element('div.tab-content:visible').text()).toContain('content 2');
+    }
 
 });
